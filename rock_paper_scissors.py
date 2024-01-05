572df3de09cb90080169bd5ed4145f26a7611256
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-05 20:15:10 +0300

    add an if function to set rules of the game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,4 +4,12 @@ def play():
     user = input("'r' for rock, 'p' for paper, 's' for scissors")
     computer = random.choice(['r', 'p', 's'])
 
+# rules to determine who wins the game
+    if user == computer:
+        return 'tie'
+    
+    # in this game we know;
+    # r > s, p > r, s > p
+
+
     
