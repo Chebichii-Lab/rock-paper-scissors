a484d10a022f6eb58489fc662d6491c8683eb74e
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-05 20:22:09 +0300

    add a helper function to determine the winner of the game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -11,5 +11,11 @@ def play():
     # in this game we know;
     # r > s, p > r, s > p
 
+# helper function to determine who wins the game
+def is_win(player, opponent):
+    #return true if player wins, false if not
+    # r > s, p > r, s > p
 
-    
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+        or (player == 'p' and opponent == 'r'):
+        return True
