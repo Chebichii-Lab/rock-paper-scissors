32a992ec4b7e05acbdef7a1462ae3e75777cce35
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-05 20:28:21 +0300

    add if statements to return responses based on the winner

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,15 +1,19 @@
 import random
 
 def play():
-    user = input("'r' for rock, 'p' for paper, 's' for scissors")
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
     computer = random.choice(['r', 'p', 's'])
 
 # rules to determine who wins the game
     if user == computer:
-        return 'tie'
+        return 'It\'s a tie'
     
     # in this game we know;
     # r > s, p > r, s > p
+    if is_win(user, computer):
+        return 'You won!'
+    
+    return 'You lost!'  # when the computer wins
 
 # helper function to determine who wins the game
 def is_win(player, opponent):
@@ -19,3 +23,6 @@ def is_win(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
         or (player == 'p' and opponent == 'r'):
         return True
+    
+
+print(play())
